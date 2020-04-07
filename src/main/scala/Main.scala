ee86a3a4a83b3ba2f99e890a76a923a7fc29954a
Kyle Prifogle <kprifogle1@gmail.com>
2020-04-07 15:19:07 -0400

    Making job argument fetching more rigorous

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,8 +6,10 @@ object Main {
   def main(args: Array[String]) {
 
     val jc = JobConfig.zero
-    val jobTypeArgs = args.take(2)
-    val otherArgs = args.drop(2)
+    val argMap = args.sliding(2).map { case Array(p1: String,p2: String) => (p1,p2) }.toList.groupBy(_._1).map { case (k,v) => (k,v.map(_._2).head)}
+    val jobTypeArgs = Array("job", argMap("job"))
+    val otherArgs = argMap.-("job").toList.flatMap{case (k,v) => List(k,v)}
+
     jc.parser.parse(jobTypeArgs, jc) match {
       case Some(config) =>
         if (config.job == "merge") {
